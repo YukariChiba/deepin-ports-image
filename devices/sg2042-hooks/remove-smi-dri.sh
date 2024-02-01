4cf6f457362ab2ac0ed2491b1da1ac8abd7d5d03
Yukari Chiba <i@0x7f.cc>
2024-02-01 16:44:14 +0800

    fix

--- devices/sg2042-hooks/remove-smi-dri.sh
+++ devices/sg2042-hooks/remove-smi-dri.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 echo "blacklist smifb" | sudo tee $ROOTFS/etc/modprobe.d/sg2042-extgpu.conf
+
+if [[ ${INCPKGS[@]} =~ initramfs-tools ]]; then
+  echo "--- Update initramfs"
+  sudo systemd-nspawn -D $ROOTFS bash -c "update-initramfs -k all -c || true"
+fi
+
--- flash/fastboot/flash_fastboot_uboot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PFX=deepin-lpi4a-riscv64
-BOOTIMG=$PFX.boot.ext4
-ROOTIMG=$PFX.root.ext4
-
-sudo fastboot flash boot $BOOTIMG
-sudo fastboot flash root $ROOTIMG
