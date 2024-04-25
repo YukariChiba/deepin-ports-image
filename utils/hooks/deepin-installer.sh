c5e81b36bc22d12ff2fc35d003be6d886ee0b756
Yukari Chiba <i@0x7f.cc>
2024-04-25 15:33:20 +0800

    add unmatched, remove unused profile

--- utils/hooks/deepin-installer.sh
+++ utils/hooks/deepin-installer.sh
@@ -4,14 +4,14 @@ if [[ ${INCPKGS[@]} =~ deepin-installer ]]; then
   echo_bold "--- deepin-installer detected, update config"
   sudo install -D ./utils/hooks/deepin-installer/deepin-installer.conf $ROOTFS/etc/deepin-installer/deepin-installer.conf
   echo -n 'apt_source_deb="' | sudo tee -a $ROOTFS/etc/deepin-installer/deepin-installer.conf
-  #if [ "$REPOPROFILE" != "stable" ]; then
-  #  for INCREPO in "${INCREPOS[@]}"
-  #  do
-  #    echo -n "$INCREPO\\n\\n" | sudo tee -a $ROOTFS/etc/deepin-installer/deepin-installer.conf
-  #  done
-  #else
+  if [ "$REPOPROFILE" != "stable" ]; then
+    for INCREPO in "${INCREPOS[@]}"
+    do
+      echo -n "$INCREPO\\n\\n" | sudo tee -a $ROOTFS/etc/deepin-installer/deepin-installer.conf
+    done
+  else
     echo -n "deb https://community-packages.deepin.com/deepin/beige/ beige main community commercial\\n\\n" | sudo tee -a $ROOTFS/etc/deepin-installer/deepin-installer.conf
-  #fi
+  fi
   echo -n '"' | sudo tee -a $ROOTFS/etc/deepin-installer/deepin-installer.conf
   echo ""
   #if [ -z $LIVE ]; then
