9b1b2fe5fa583a998734ac7783fd7e5d69821836
Yukari Chiba <i@0x7f.cc>
2024-05-28 17:10:29 +0800

    force copy spacemit firmware to initramfs

---
+++ devices/musebox-hooks/boot-firmware.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cat <<EOF | sudo tee $ROOTFS/etc/initramfs-tools/hooks/copy-spacemit-firmware
+#!/bin/sh
+
+set -e
+
+PREREQ=""
+
+prereqs()
+{
+        echo "\${PREREQ}"
+}
+
+case "\${1}" in
+        prereqs)
+                prereqs
+                exit 0
+                ;;
+esac
+
+. /usr/share/initramfs-tools/hook-functions
+
+mkdir -p \${DESTDIR}/lib/firmware
+cp -a /lib/firmware/esos.elf \${DESTDIR}/lib/firmware
+EOF
+
+chmod +x $ROOTFS/etc/initramfs-tools/hooks/copy-spacemit-firmware
