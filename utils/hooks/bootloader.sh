adf5768e44ed7f1d1cf904594e0a6f7224674036
Yukari Chiba <i@0x7f.cc>
2024-05-28 14:22:10 +0800

    disable extlinux for musebox

--- utils/hooks/bootloader.sh
+++ utils/hooks/bootloader.sh
@@ -63,3 +63,10 @@ if [ "$BOOTLOADER" == "grub" ]; then
     fi
   fi
 fi
+
+if [ -z $BOOTLOADER ]; then
+  if [[ ${INCPKGS[@]} =~ initramfs-tools ]]; then
+    echo "--- Update initramfs"
+    sudo systemd-nspawn -D $ROOTFS bash -c "update-initramfs -k all -c || true"
+  fi
+fi
