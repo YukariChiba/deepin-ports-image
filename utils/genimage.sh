5735e464088348abac208bcd4580cbd20896d951
Yukari Chiba <i@0x7f.cc>
2024-06-03 17:38:05 +0800

    fix genimage

--- utils/genimage.sh
+++ utils/genimage.sh
@@ -7,16 +7,12 @@ if [ ! -z $IMGPROFILE ]; then
     GENIMGCFG=`mktemp ./tmp/tmp.genimage.XXXXXX`
     GENIMGTMP=`mktemp -d ./tmp/tmp.genimage.XXXXXX`
     cp ./genimage/$IMGPROFILE.cfg $GENIMGCFG
-    sed -i "s@%blpfx%@../injectbin/bootloader/$TARGET_DEVICE@" $GENIMGCFG
-    sed -i "s/%image%/$IMGPFX.img/" $GENIMGCFG
-    sed -i "s/%boot%/$BOOTIMG/" $GENIMGCFG
-    sed -i "s/%root%/$DISKIMG/" $GENIMGCFG
-    GENIMGINC="--inputpath results"
-    if [ -d ./injectbin/bootloader/$TARGET_DEVICE ]; then
-      GENIMGINC+=" --includepath ./injectbin/bootloader/$TARGET_DEVICE"
-    fi
+    sed -i "s@%blpfx%@injectbin/bootloader/$TARGET_DEVICE@" $GENIMGCFG
+    sed -i "s@%image%@$IMGPFX.img@" $GENIMGCFG
+    sed -i "s@%boot%@$BOOTIMG@" $GENIMGCFG
+    sed -i "s@%root%@$DISKIMG@" $GENIMGCFG
     genimage --config $GENIMGCFG \
-      $GENIMGINC \
+      --inputpath . \
       --tmppath $GENIMGTMP \
       --outputpath results-img
   else
