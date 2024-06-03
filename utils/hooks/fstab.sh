d772efdee2309c917a70313a969baf7961fffed5
Yukari Chiba <i@0x7f.cc>
2024-06-03 12:07:00 +0800

    fix resize rootpart

--- utils/hooks/fstab.sh
+++ utils/hooks/fstab.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+if [ -z $NOGROWROOT ]; then
+  cat <<EOF | sudo tee -a $ROOTFS/etc/fstab
+LABEL=root / ext4 defaults,rw,errors=remount-ro,x-systemd.growfs 0 0
+EOF
+fi
+
 if [ "$TARGET_DEVICE" == "romadc" ]; then
-  cat <<EOF | sudo tee $ROOTFS/etc/fstab
+  cat <<EOF | sudo tee -a $ROOTFS/etc/fstab
 /dev/mmcblk1p3 /boot vfat defaults 0 0
 EOF
 fi
 
-if [ "$TARGET_DEVICE" == "lpi4a" ]; then
-  cat <<EOF | sudo tee $ROOTFS/etc/fstab
+if [ "$BOOTFMT" == "ext4" ] && [ "$BOOTSIZE" -ne "0" ]; then
+  cat <<EOF | sudo tee -a $ROOTFS/etc/fstab
 LABEL=boot /boot ext4 defaults 0 0
 EOF
 fi
--- utils/hooks/grow-root.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -z $NOGROWROOT ]; then
-sudo mkdir -p $ROOTFS/etc/repart.d/
-cat <<EOF | sudo tee $ROOTFS/etc/repart.d/resize-root.conf > /dev/null
-[Partition]
-Type=root
-EOF
-fi
