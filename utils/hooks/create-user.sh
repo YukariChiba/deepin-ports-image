860b2ae145e08bb3cf633f4b4734e612226768dc
Yukari Chiba <i@0x7f.cc>
2024-06-04 10:57:29 +0800

    set root password

--- utils/hooks/create-user.sh
+++ utils/hooks/create-user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#sudo systemd-nspawn -D $ROOTFS bash -c "(echo root:deepin | chpasswd) || true"
+sudo systemd-nspawn -D $ROOTFS bash -c "(echo root:deepin | chpasswd) || true"
 
 if [[ ! ${INCPKGS[@]} =~ deepin-installer ]]; then
   sudo systemd-nspawn -D $ROOTFS bash -c "(useradd -m -g users deepin || true) && (usermod -a -G sudo deepin || true)"
