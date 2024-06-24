772066ff354b7ffbd008b171b9868ad890a28486
Yukari Chiba <i@0x7f.cc>
2024-06-24 14:38:57 +0800

    do not repart on phytiumpi

--- utils/hooks/repart.sh
+++ utils/hooks/repart.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-sudo mkdir -p $ROOTFS/etc/repart.d
-
-cat <<EOF | sudo tee $ROOTFS/etc/repart.d/growroot.conf
+# phytium pi: MBR
+if [ "$TARGET_DEVICE" != "phytiumpi" ]; then
+  sudo mkdir -p $ROOTFS/etc/repart.d
+  cat <<EOF | sudo tee $ROOTFS/etc/repart.d/growroot.conf
 [Partition]
 Type=linux-generic
 GrowFileSystem=true
 EOF
+
+fi
