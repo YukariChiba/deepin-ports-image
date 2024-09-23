715bf871b35822633d7799a1c56e2dba596cd5d9
Yukari Chiba <i@0x7f.cc>
2024-09-23 16:34:53 +0800

    tmp set p1 to use pvrdri

--- devices/th1520-hooks/imggpu.sh
+++ devices/th1520-hooks/imggpu.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+if [ "$TARGET_DEVICE" == "p1" ]; then
+
+cat <<EOF | sudo tee $ROOTFS/etc/X11/xorg.conf.d/99-imggpu.conf > /dev/null
+Section "Device"
+        Identifier "imggpu"
+        Driver "pvrdri"
+	Option "UseGammaLUT" "false"
+        Option "kmsdev" "/dev/dri/card0"
+EndSection
+EOF
+
+else
+
 cat <<EOF | sudo tee $ROOTFS/etc/X11/xorg.conf.d/99-imggpu.conf > /dev/null
 Section "Device"
         Identifier "imggpu"
@@ -7,3 +20,5 @@ Section "Device"
         Option "kmsdev" "/dev/dri/card0"
 EndSection
 EOF
+
+fi
