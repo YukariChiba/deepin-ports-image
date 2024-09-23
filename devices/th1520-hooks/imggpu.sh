6513014c2977643f765379edfd7f867826f98c44
Yukari Chiba <i@0x7f.cc>
2024-09-23 17:25:51 +0800

    p1: shrink bootsize, remove some extrapkgs, revert tmp g2d fix

--- devices/th1520-hooks/imggpu.sh
+++ devices/th1520-hooks/imggpu.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 
-if [ "$TARGET_DEVICE" == "p1" ]; then
-
-cat <<EOF | sudo tee $ROOTFS/etc/X11/xorg.conf.d/99-imggpu.conf > /dev/null
-Section "Device"
-        Identifier "imggpu"
-        Driver "pvrdri"
-	Option "UseGammaLUT" "false"
-        Option "kmsdev" "/dev/dri/card0"
-EndSection
-EOF
-
-else
-
 cat <<EOF | sudo tee $ROOTFS/etc/X11/xorg.conf.d/99-imggpu.conf > /dev/null
 Section "Device"
         Identifier "imggpu"
         Driver "thead"
+	Option "UseGammaLUT" "false"
         Option "kmsdev" "/dev/dri/card0"
 EndSection
 EOF
-
-fi
