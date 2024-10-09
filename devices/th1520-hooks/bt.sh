e08d8288a990308844c4acd77ff7cdecd85214c3
Yukari Chiba <i@0x7f.cc>
2024-10-09 14:12:52 +0800

    fix bluetooth for rvbook, add zhihe variant

--- devices/th1520-hooks/bt.sh
+++ devices/th1520-hooks/bt.sh
@@ -4,6 +4,10 @@ sudo cp ./devices/$TARGET_DEVICE-hooks/bt/rtk_hciattach $ROOTFS/usr/bin/rtk_hcia
 sudo chmod 755 $ROOTFS/usr/bin/rtk_hciattach
 sudo cp ./devices/$TARGET_DEVICE-hooks/bt/rtl8* $ROOTFS/lib/firmware/rtlbt/
 sudo cp ./devices/$TARGET_DEVICE-hooks/bt/light-bt.service $ROOTFS/lib/systemd/system/bt-init.service
-sudo cp ./devices/$TARGET_DEVICE-hooks/bt/bt-init $ROOTFS/usr/bin/bt-init
+if [ "$TARGET_DEVICE" == "rvbook" ]; then
+  sudo cp ./devices/$TARGET_DEVICE-hooks/bt/bt-init-zhihe $ROOTFS/usr/bin/bt-init
+else
+  sudo cp ./devices/$TARGET_DEVICE-hooks/bt/bt-init $ROOTFS/usr/bin/bt-init
+fi
 sudo chmod 755 $ROOTFS/usr/bin/bt-init
 sudo ln -s $ROOTFS/lib/systemd/system/bt-init.service $ROOTFS/etc/systemd/system/multi-user.target.wants/bt-init.service
