3f12034e196178e3b90fdff91cd94daf762f4b4d
Yukari Chiba <i@0x7f.cc>
2024-11-19 19:45:06 +0800

    update deviceinfo for zhihe

--- devices/th1520-hooks/zhihe/deviceinfo.sh
+++ devices/th1520-hooks/zhihe/deviceinfo.sh
@@ -6,8 +6,8 @@ cat <<EOF >/etc/deviceinfo.ini
 [device]
 OS_NAME=$PRETTY_NAME
 OS_VERSION=`cat /etc/os-version | grep OsBuild | cut -f2 -d '='`
-DEVICE_BRAND=
-DEVICE_MODEL=
-DEVICE_SN=
-DEVICE_UUID=
+DEVICE_BRAND=`fw_printenv DEVICE_BRAND || :`
+DEVICE_MODEL=`fw_printenv DEVICE_MODEL || :`
+DEVICE_SN=`fw_printenv DEVICE_SN || :`
+DEVICE_UUID=`fw_printenv DEVICE_UUID || :`
 EOF
