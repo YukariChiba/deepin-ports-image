08aeb6faf0e57ff0fcf6c0f841fcbc672532ea98
Yukari Chiba <i@0x7f.cc>
2024-11-22 13:53:43 +0800

    adapt for v25

--- utils/hooks/deepin-installer.sh
+++ utils/hooks/deepin-installer.sh
@@ -3,6 +3,7 @@
 if [[ ${INCPKGS[@]} =~ deepin-installer ]]; then
   echo_bold "--- deepin-installer detected, update config"
   sudo install -D ./utils/hooks/deepin-installer/deepin-installer.conf $ROOTFS/etc/deepin-installer/deepin-installer.conf
+
   echo -n 'apt_source_deb="' | sudo tee -a $ROOTFS/etc/deepin-installer/deepin-installer.conf
   for INCREPO in "${INCREPOS[@]}"
   do
@@ -10,6 +11,15 @@ if [[ ${INCPKGS[@]} =~ deepin-installer ]]; then
   done
   echo -n '"' | sudo tee -a $ROOTFS/etc/deepin-installer/deepin-installer.conf
   echo ""
+
+  echo -n 'DI_APT_SOURCE_DEB="' | sudo tee -a $ROOTFS/etc/deepin-installer/deepin-installer.conf
+  for INCREPO in "${INCREPOS[@]}"
+  do
+    echo -n "$INCREPO\\n\\n" | sudo tee -a $ROOTFS/etc/deepin-installer/deepin-installer.conf > /dev/null
+  done
+  echo -n '"' | sudo tee -a $ROOTFS/etc/deepin-installer/deepin-installer.conf
+  echo ""
+
   sudo ln -s ../deepin-installer-first-boot.service $ROOTFS/usr/lib/systemd/system/multi-user.target.wants/deepin-installer-first-boot.service
   sudo rm $ROOTFS/usr/lib/systemd/system/deepin-installer.service
 fi
