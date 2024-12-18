966a07fba61cc5f016489e7496f1a3e571a4880b
Yukari Chiba <i@0x7f.cc>
2024-12-18 17:35:20 +0800

    zhihe: add chromium, fix bt filename

--- devices/th1520-hooks/zhihe.sh
+++ devices/th1520-hooks/zhihe.sh
@@ -7,4 +7,7 @@ if [ "$TARGET_DEVICE" == "p1" ] || [ "$TARGET_DEVICE" == "rvbook-zhihe" ]; then
   sudo chmod +x $ROOTFS/usr/bin/update-deviceinfo
   sudo cp ./devices/$TARGET_DEVICE-hooks/zhihe/update-deviceinfo.service $ROOTFS/usr/lib/systemd/system/update-deviceinfo.service
   sudo ln -s $ROOTFS/lib/systemd/system/update-deviceinfo.service $ROOTFS/etc/systemd/system/multi-user.target.wants/update-deviceinfo.service
+
+  # use chromium, remove firefox
+  sudo systemd-nspawn -D $ROOTFS bash -c "apt purge -y firefox"
 fi
