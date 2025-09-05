4e9d2eca22f599702ecca7fe501d86aee53c7fa3
Yukari Chiba <i@0x7f.cc>
2025-09-05 15:09:27 +0800

    fix linglong install

--- utils/hooks/linglong.sh
+++ utils/hooks/linglong.sh
@@ -3,13 +3,13 @@
 if [ -f $ROOTFS/usr/bin/ll-cli ]; then
 
   if [ ! -z $INTERNAL_REPO ]; then
-    sudo systemd-nspawn -D $ROOTFS bash -c "ll-cli repo add stable-internal https://repo-dev.cicd.getdeepin.org && ll-cli repo set-priority stable-internal 999"
+    sudo systemd-nspawn -D $ROOTFS bash -c "ll-cli --no-dbus repo add stable-internal https://repo-dev.cicd.getdeepin.org && ll-cli repo set-priority stable-internal 999"
   fi
 
-  cat $ROOTFS/etc/packages.linglong.* | grep -v '#' | sudo tee $ROOTFS/tmp/ll.list
-  sudo systemd-nspawn -D $ROOTFS bash -c "xargs -n1 --arg-file=/tmp/ll.list ll-cli --no-dbus install"
+  cat $ROOTFS/etc/packages.linglong.* | grep -v '#' | sudo tee $ROOTFS/etc/packages.linglong
+  sudo systemd-nspawn -D $ROOTFS bash -c "xargs -n1 --arg-file=/etc/packages.linglong ll-cli --no-dbus install"
 
   if [ ! -z $INTERNAL_REPO ]; then
-    sudo systemd-nspawn -D $ROOTFS bash -c "ll-cli repo remove stable-internal"
+    sudo systemd-nspawn -D $ROOTFS bash -c "ll-cli --no-dbus repo remove stable-internal"
   fi
 fi
