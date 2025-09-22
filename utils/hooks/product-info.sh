0b8d7b0d8503c24c08e72d5ce38ed4443d788e04
Yukari Chiba <i@0x7f.cc>
2025-09-22 12:06:42 +0800

    fix product info generation

--- utils/hooks/product-info.sh
+++ utils/hooks/product-info.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 cat <<EOF | sudo tee $ROOTFS/etc/product-info > /dev/null
-Deepin OS-$REPOPROFILE-$(date "+%Y%m%d%H%M%S")00-1_$TARGET_ARCH
+$(cat $ROOTFS/etc/os-version  | grep "SystemName=")
+$(cat $ROOTFS/etc/os-version  | grep "EditionName=")
+$(cat $ROOTFS/etc/os-version  | grep "MajorVersion=")
+$(cat $ROOTFS/etc/os-version  | grep "MinorVersion=")
+ProductName=Community
+BuildTime=$(TZ="Asia/Shanghai" date +%Y%m%d%H%M%S)
+Arch=$TARGET_ARCH
+OEM=
 EOF
 
