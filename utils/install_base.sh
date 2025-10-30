2f8148ca379ee8e89ad2b270b8679ebcc8641c61
Yukari Chiba <i@0x7f.cc>
2025-10-30 15:43:17 +0800

    update linglong internal repo, update doc

--- utils/install_base.sh
+++ utils/install_base.sh
@@ -49,7 +49,7 @@ sudo systemd-nspawn -D $ROOTFS bash -c "export DEBIAN_FRONTEND=noninteractive &&
 if [ -f $ROOTFS/usr/bin/ll-cli ]; then
 
   if [ ! -z $INTERNAL_REPO ]; then
-    sudo systemd-nspawn -D $ROOTFS bash -c "ll-cli --no-dbus repo update stable https://mirror-repo-linglong.uniontech.com"
+    sudo systemd-nspawn -D $ROOTFS bash -c "ll-cli --no-dbus repo update stable https://pools.uniontech.com/linglong/"
   fi
 
   cat $ROOTFS/etc/packages.linglong.* | grep -v '#' | sudo tee $ROOTFS/etc/packages.linglong
